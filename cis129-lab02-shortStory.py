644e823a0c637040a698ca544864204c9ac943fe
Cade274 <159203359+Cade274@users.noreply.github.com>
2024-02-09 00:00:54 -0700

    Update cis129-lab02-shortStory.py
    - Deleted 'Hello World!' statement. 
    - Added short story (not based on real life)
    - Added header

--- cis129-lab02-shortStory.py
+++ cis129-lab02-shortStory.py
@@ -1,1 +1,8 @@
-print('Hello World!')
+# Author: Rylan Carney
+# My first Python lab in CIS129
+# Prints a simple homemade short story
+print('If life were a video game, it would be so easy for me!')
+print('My hands are fast, my reflexes quick!')
+print('Puzzles and problems, their solutions I can easily see!')
+print('However, sad to say, even geared with my hat and my bat,')
+print('The police would not let me farm for EXP.')
